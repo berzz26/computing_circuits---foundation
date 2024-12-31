bd3466cd3bae9f2a46bc3396ab2ff331352df0ae
berzz26 <aumtamboli2233@gmail.com>
2024-12-31 12:47:30 +0530

    initial commit

---
+++ Gates/main.cpp
@@ -0,0 +1,28 @@
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int AND () {
+
+}
+int OR () {
+
+}
+int NOT () {
+
+}
+int XOR(){
+
+}
+int NAND() {
+
+}
+int NOR(){
+
+}
+
+int main() {
+
+ 
+}
